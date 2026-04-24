e1dbcf423155a1fac4530647fdd58c1bcd927ef7
H. Sanichar <hsanichar2006@gmail.com>
2026-04-24 00:21:42 -0400

    completed sqaure.js

--- square.js
+++ square.js
@@ -1,36 +1,60 @@
-let squareArea = document.querySelector("#squareArea");
-//let squareArea = document.getElementById("squareArea");
-let squareCount = parseInt((Math.random()* 21)+ 30); //30-50
-//Make some variable for a really big zIndex
-for(let i = 0; i < squareCount; i++){
-    let square = document.createElement("div");
-    square.className = "square";
-    square.style.left = parseInt(Math.random() * 650) + "px";
-    square.style.top = parseInt(Math.random() * 250) + "px";
-    square.style.backgroundColor = getRandomColor();
-    //I can put a oneclick and a function(this)
-    //put it on screen
-    squareArea.append(square);
-}
+let squareArea = document.querySelector("#squarearea"); 
+//let squareArea = document.getElementById("squareArea"); 
+let squareCount = parseInt((Math.random()* 21)+ 30); //30-50 
 
-function getRandomColor(){
-    //Make a color string?
-    let letters = "0123456789ABCDEF";
-    let color = "#";
-    for(let i = 0; i < 6; i++){
-        color+=letters.charAt(parseInt(Math.random()*letters.length));
+//Variable for managing z-index (bringing squares to front)
+let zIndexCounter = 1; 
+
+//Updated onload that uses addSquare()
+window.onload = function(){
+    for(let i = 0; i < squareCount; i++){ 
+        addSquare(); //calling our function instead of writing code here
     }
-    //return that string
-    return color;
 }
-//Make a function to add 1 square
-//Make sure that I set that as the oneclick for thebutton
 
-//Make a function that changes all the colors of all the sqaures 
-//make sure the button calls it
-function changeColors(){
-    //Make an array 
-    let squares = document.querySelectorAll(".square");
-    //For of loop or just do a regular for loop with index
-    //Change the color 
+//Function that adds a square (connected to button in HTML)
+function addSquare(){
+    let square = document.createElement("div"); 
+    square.className = "square"; 
+    square.style.left = parseInt(Math.random() * 650) + "px"; 
+    square.style.top = parseInt(Math.random() * 250) + "px"; 
+    square.style.backgroundColor = getRandomColor(); 
+
+    //Randomize square size (45–55 pixels)
+    let size = parseInt(Math.random() * 11) + 45; 
+    square.style.width = size + "px"; 
+    square.style.height = size + "px"; 
+
+    //onclick behavior
+    square.onclick = function(){
+        //If square is already on top, delete it
+        if(square.style.zIndex == zIndexCounter){
+            square.remove();
+        } 
+        else{
+            //Otherwise bring it to the front
+            zIndexCounter++;
+            square.style.zIndex = zIndexCounter;
+        }
+    }
+    squareArea.append(square); 
+}
+
+//Function for random colors
+function getRandomColor(){ 
+
+    let letters = "0123456789ABCDEF"; 
+    let color = "#"; 
+    for(let i = 0; i < 6; i++){ 
+        color += letters.charAt(parseInt(Math.random()*letters.length)); 
+    } 
+    return color; 
+} 
+
+//Function that changes all square colors
+function changeColors(){ 
+    let squares = document.querySelectorAll(".square"); 
+    for(let i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = getRandomColor();
+    }
 }
